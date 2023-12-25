c59c51685143104486ee9b8b3ab8075a630e3b21
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2023-12-26 03:28:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (5.10%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,62 @@
+class MyQueue {
+
+    Stack <Integer> myStackQ = new Stack<>();
+    Stack <Integer> myHelper = new Stack<>();
+
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+
+        myStackQ.push(x);
+       
+    }
+    
+    public int pop() {
+        while(!myStackQ.isEmpty())
+        {
+            myHelper.push(myStackQ.pop());
+        } // helper stack now has all values reversed
+
+        int popped =  myHelper.pop();
+        //first element removed
+
+        while(!myHelper.isEmpty())
+        {
+            myStackQ.push(myHelper.pop());
+        } // queue is set again in main stack
+
+        return popped;
+    }
+    
+    public int peek() {
+        while(!myStackQ.isEmpty())
+        {
+            myHelper.push(myStackQ.pop());
+        } // helper stack now has all values reversed
+
+        int peekValue = myHelper.peek();
+        //first element peeked
+
+        while(!myHelper.isEmpty())
+        {
+            myStackQ.push(myHelper.pop());
+        } // queue is set again in main stack
+
+        return peekValue;
+    }
+    
+    public boolean empty() {
+        return myStackQ.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
