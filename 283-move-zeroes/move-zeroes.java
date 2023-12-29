2ad94162cdcee6e34d31facae0ed94b15fa57fff
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2023-12-30 03:34:50 +0530

    Time: 24 ms (11.26%) | Memory: 45.5 MB (23.33%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,20 +1,42 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 
-        int countZero = 0;
-        //we maintain a zerocounter and do changes accordingly
-        for(int i = 0; i<nums.length ; i++)
+        //not optimized but simple for beiginners
+        // int countZero = 0;
+        // //we maintain a zerocounter and do changes accordingly
+        // for(int i = 0; i<nums.length ; i++)
+        // {
+        //     if(nums[i] == 0)
+        //     {
+        //         countZero++;
+        //     }
+        //     else if(countZero>0)
+        //     {
+        //         int t = nums[i];
+        //         nums[i] = 0;
+        //         nums[i-countZero] = t;
+        //     }
+        // }
+
+
+        //optimized two pointer
+        int readptr = 0;
+        int writeptr = 0;
+        while(readptr < nums.length)
         {
-            if(nums[i] == 0)
+            if(nums[readptr] != 0)
             {
-                countZero++;
+                nums[writeptr] = nums[readptr];
+                writeptr++;
             }
-            else if(countZero>0)
-            {
-                int t = nums[i];
-                nums[i] = 0;
-                nums[i-countZero] = t;
-            }
+
+            readptr++;
+        }
+
+        while(writeptr < nums.length)
+        {
+            nums[writeptr] = 0;
+            writeptr++;
         }
 
         System.out.println(Arrays.toString(nums));
