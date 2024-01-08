eaab5246f9b16a4de2c2fa4b7d31f456277359b3
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-01-08 05:42:40 +0530

    Time: 1 ms (30.70%) | Memory: 42.8 MB (32.61%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,32 +1,46 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int ind =-1;
-        int n= nums.length;
-        for(int i = n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind=i;
+        int breakPointIndex = -1;
+        int n = nums.length;
+
+        //finding out breakpoint
+        for(int i = n-2; i >= 0; i--)
+        {
+            if(nums[i] < nums[i+1])
+            {
+                breakPointIndex = i;
                 break;
             }
         }
-        if(ind==-1){
-            int j = nums.length-1;
-            for(int i=0;i<nums.length/2;i++){
-                int t=nums[i];
-                nums[i]=nums[j];
-                nums[j]=t;
-                j--;
+
+        if(breakPointIndex == -1)
+        {
+            //last permutation so we need teh first permutation --> simply reverse     
+            int j = nums.length - 1;
+            for(int i = 0; i < n/2; i++)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j--;            
             }
         }
-        else{
-            for(int i=n-1;i>=ind;i--){
-                if(nums[i]>nums[ind]){
-                    int t= nums[i];
-                    nums[i]=nums[ind];
-                    nums[ind]=t;
+        else
+        {
+            //find the smallest integer greater than the breakpoint integer
+            for(int i = nums.length - 1; i >= breakPointIndex ; i--)
+            {
+                if(nums[i] > nums[breakPointIndex])
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[breakPointIndex];
+                    nums[breakPointIndex] =temp;
                     break;
                 }
             }
-            Arrays.sort(nums,ind+1,n);
         }
+
+        //arrange the permutation
+        Arrays.sort(nums, breakPointIndex+1, n);
     }
 }
