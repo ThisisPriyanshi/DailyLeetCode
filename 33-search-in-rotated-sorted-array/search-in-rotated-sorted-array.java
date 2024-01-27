b6e1c3b11133eda047513bbbf6214593c416ff8c
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-01-28 00:18:22 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (30.59%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,35 +1,48 @@
 class Solution {
-    public int search(int[] arr, int k) {
-int low = 0, high = arr.length - 1;
-        while (low <= high) {
+    public int search(int[] nums, int target) {
+        //step 1 - set low, high, mid.
+        //step 2 - find the sorted side 
+        //step 3 - check if the sorted side has the target
+        //step 4 - change low high accordingly.
+
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low <= high)
+        {
             int mid = (low + high) / 2;
-
-            // if mid points to the target
-            if (arr[mid] == k)
-                return mid;
-
-            // if left part is sorted
-            if (arr[low] <= arr[mid]) 
+            //check if mid is target
+            if(nums[mid] == target)
             {
-                if ((arr[low] <= k && k <= arr[mid])) {
-                    // element exists
+                return mid;
+            }
+            //find sorted half
+            //check if left-half is sorted
+            if (nums[low] <= nums[mid])
+            {
+                //check if target lies there
+                if( (nums[low] <= target) && ( target <= nums[mid]))
+                {
                     high = mid - 1;
-                } else {
-                    // element does not exist
+                }
+                else
+                {
                     low = mid + 1;
                 }
-            } 
-            else 
-            { // if right part is sorted
-                if (arr[mid] <= k && k <= arr[high]) {
-                    // element exists
+            }
+            else
+            {
+                //if right part is sorted
+                if((nums[mid] <= target) && (target <= nums[high]))
+                {
                     low = mid + 1;
-                } else {
-                    // element does not exist
+                }
+                else
+                {
                     high = mid - 1;
                 }
             }
         }
-        return -1;
+        return -1; 
     }
 }
