d414ff3bde7b94588033c5bf191af02b9aa2f402
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-02-23 00:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (86.73%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,46 +1,44 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int breakPointIndex = -1;
-        int n = nums.length;
-
-        //finding out breakpoint
-        for(int i = n-2; i >= 0; i--)
-        {
-            if(nums[i] < nums[i+1])
-            {
-                breakPointIndex = i;
-                break;
-            }
-        }
-
-        if(breakPointIndex == -1)
-        {
-            //last permutation so we need the first permutation --> simply reverse     
-            int j = nums.length - 1;
-            for(int i = 0; i < n/2; i++)
-            {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j--;            
-            }
-        }
-        else
-        {
-            //find the smallest integer greater than the breakpoint integer
-            for(int i = nums.length - 1; i >= breakPointIndex ; i--)
-            {
-                if(nums[i] > nums[breakPointIndex])
+       int n = nums.length;
+        int index =-1;
+        int i=0,j=0,temp=0;
+            for( i = n-2; i>=0; i--){
+                if(nums[i] < nums[i+1])
                 {
-                    int temp = nums[i];
-                    nums[i] = nums[breakPointIndex];
-                    nums[breakPointIndex] =temp;
+                    index=i;
                     break;
                 }
             }
+            if( index != -1){
+                for( i = n-1; i>index; i--){
+                    if( nums[i] > nums[index]){
+                        temp = nums[i];
+                        nums[i] = nums[index];
+                        nums[index] = temp;
+                        break;
+                    }
+                }
+                i = index+1;
+                j = nums.length-1;
+                while( i < j ){
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    i++;
+                    j--;
+                }
+            }else{
+                i = 0;
+                j = nums.length-1;
+                while( i < j ){
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    i++;
+                    j--;
+                }
         }
-
-        //arrange the permutation
-        Arrays.sort(nums, breakPointIndex+1, n);
     }
+
 }
