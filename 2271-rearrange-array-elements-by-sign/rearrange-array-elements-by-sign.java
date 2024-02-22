d6e3a5c140717839f7027b85dfd3a9e52aa20c5f
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-02-23 00:57:56 +0530

    Time: 3 ms (100.00%) | Memory: 76.5 MB (84.43%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         
-        int n = nums.length;
-        int posIndex = 0, negIndex = 0;
-        int[] result = new int[n];
-
-        // Place positive numbers at even indices and negative numbers at odd indices
-        for (int i = 0; i < n; ++i) {
-            if (i % 2 == 0) {
-                while (posIndex < n && nums[posIndex] < 0)
-                    ++posIndex;
-                result[i] = nums[posIndex++];
-            } else {
-                while (negIndex < n && nums[negIndex] >= 0)
-                    ++negIndex;
-                result[i] = nums[negIndex++];
+int[] result = new int[nums.length];
+        int i = 0;
+        int j = 1;
+        for(int num : nums) {
+            // 1. fill positive integers
+            if(num >= 0) {
+                result[i] = num;
+                i += 2;
+            }
+            // 2. fill negative integers
+            else {
+                result[j] = num;
+                j += 2;
             }
         }
         return result;
