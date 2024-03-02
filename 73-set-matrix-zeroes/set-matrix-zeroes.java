c855e4045063907ce80470b5e7fa94c238a9e41c
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-03-03 03:11:34 +0530

    Time: 1 ms (75.38%) | Memory: 45.4 MB (65.68%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,38 +1,64 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        
+        //space complexity optimised solution
+
         int rows = matrix.length;
         int columns = matrix[0].length;
 
-        int[] rowMarker = new int[rows];
-        int[] columnsMarker = new int[columns];
-        //by default arrays will be initiated with 0
-
-        for(int i = 0 ;i < rows; i++)
+        int colZero = 1;
+        //do the marking
+        for(int i = 0; i < rows; i++)
         {
-            for (int j = 0; j < columns; j++)
+            for(int j = 0 ; j < columns; j++)
             {
                 if(matrix[i][j] == 0)
                 {
-                    rowMarker[i] = 1;
-                    columnsMarker[j] = 1;
+                    //mark the iTh row
+                    matrix[i][0] = 0;
+
+                    //mark the jTh column
+                    if(j != 0)
+                    matrix[0][j] = 0;
+                    else
+                    colZero = 0;
                 }
             }
         }
-        
-        //it is marked
 
-        for(int k = 0; k < rows; k++)
+        //we have marked in the zero row and column
+        //now we change all values but not from the 0th row and column
+
+        for(int i = 1; i < rows; i++)
         {
-            for(int l = 0; l < columns; l++)
+            for(int j = 1; j < columns; j++)
             {
-                if((rowMarker[k] == 1) || (columnsMarker[l] == 1))
+                if(matrix[i][j] != 0)
                 {
-                    matrix[k][l] = 0;
+                    //check for col and row
+                    if( matrix[i][0] == 0 || matrix[0][j] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
                 }
             }
         }
 
-        // all marked as zeroes
+        //now first we take care of the zeroth row
+
+        if(matrix[0][0] == 0)
+        {
+            for(int j = 0; j < columns; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(colZero == 0)
+        {
+            for(int i = 0; i < rows; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
