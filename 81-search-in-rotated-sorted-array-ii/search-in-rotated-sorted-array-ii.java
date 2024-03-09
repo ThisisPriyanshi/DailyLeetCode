aaa141fae0d416daf524a0a133fc14a45e871f0a
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-03-10 00:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (89.26%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,29 +1,49 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-int low = 0, high = nums.length - 1;
+        int low = 0;
+        int high = nums.length - 1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] == target) return true;
+        while(low <= high)
+        {
+            int mid = (low+high)/2;
 
-            if (nums[low] == nums[mid] && nums[mid] == nums[high])           {
+            if(nums[mid] == target)
+            return true;
+
+            //check for duplicates due to repeated elements and one pivot
+
+            if((nums[low] == nums[mid]) && (nums[mid] == nums[high]))
+            {
                 low = low + 1;
                 high = high - 1;
                 continue;
             }
 
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target <= nums[mid]) 
-                high = mid - 1;
-                else 
-                low = mid + 1;
-            } else {
-                if (nums[mid] <= target && target <= nums[high]) 
-                low = mid + 1;
-                else 
-                high = mid - 1;
+            if(nums[low] <= nums[mid])
+            {
+                //left side is sorted
+                if((nums[low] <= target) && (target <= nums[mid]))
+                {
+                    high = mid-1;
+                }
+                else
+                {
+                    low = mid+1;
+                }
+            }
+            else //right side is sorted
+            {
+                if((nums[mid] <= target) && (target <= nums[high]))
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
             }
         }
+
         return false;
     }
 }
