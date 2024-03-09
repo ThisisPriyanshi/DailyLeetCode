97a27b2796697f4ab2145d2bcc854555614b97b2
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-03-09 17:05:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (29.80%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,27 +1,24 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        //sorted array looking for a target and complexity has to be O(LOG N) so we use binary Search
+
         int start = 0;
-        int end = nums.length - 1;
-        int mid = 0;
-
-        while(start <= end)
+        int end = nums.length-1;
+        while( start <= end)
         {
-            mid= start + (end-start) / 2;
-
-            if(nums[mid] == target)
-            {
-                return mid;
-            }
-            else if (nums[mid] < target)
-            {
-                start = mid+1;
-            }
-            else
-            {
-                end = mid-1;
-            }
+            int mid = start + (end - start)/2;
+             if(target == nums[mid])
+             return mid;
+             else if ( nums[mid] < target)
+             {
+                 start = mid+1;
+             }
+             else
+             {
+                 end = mid-1;
+             }
         }
-
+        
         return start;
     }
 }
