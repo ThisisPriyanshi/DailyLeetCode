3289dd424368c2d92fb2a618a9bde9ec3569e05b
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-03-09 15:36:38 +0530

    Time: 0 ms (100.00%) | Memory: 62.3 MB (88.00%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -3,6 +3,11 @@ class Solution {
         int i = 0;
         int j = 0;
 
+        //optimization for cases
+        if (nums1[nums1.length-1] < nums2[0] || nums2[nums2.length-1] < nums1[0])
+            return -1;
+
+
         while( i < nums1.length && j < nums2.length)
         {
             if(nums1[i] == nums2[j])
