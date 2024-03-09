f0d93ddb3b429c0846870a517cdf8d7f3a6bc04e
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-03-10 01:34:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (59.20%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,37 +1,40 @@
 class Solution {
     public int findMin(int[] nums) {
-        //set low, high, mid, ans
-        //find out sorted parted --> first valueof the sorted could be a possible answer
-        //check if the other part has the possible answer
-        
+        //step 1 - set low, high, mid and a variable ans to store the answer
+        //step 2 - check 3 cases --> array is not pivoted, left half is sorted, right half is sorted
+        //step 3 - whichever case maybe true --> set values by using Math.min(ans, first element of the case)
+        //return ans
+
         int low = 0;
         int high = nums.length - 1;
         int ans = Integer.MAX_VALUE;
+
         while(low <= high)
         {
-            int mid = (low + high) / 2;
+            int mid = (low+high)/2;
 
-            //check if array is sorted in itself(not rotated or rotated to original position)
+            //case 1 - array is sorted, not pivoted
             if(nums[low] < nums[high])
             {
                 ans = Math.min(ans, nums[low]);
                 break;
             }
-
-            //find out the sroted half and set 
+            
+            //case 2 - left side is sorted
             if(nums[low] <= nums[mid])
             {
                 ans = Math.min(ans, nums[low]);
-                low = mid+1;
+                low = mid + 1;
             }
-            
-            if(nums[mid] <= nums[high])
+
+            //case 3 - right side sorted
+            if(nums[mid] < nums[high])
             {
                 ans = Math.min(ans, nums[mid]);
                 high = mid - 1;
             }
-        }
 
-        return ans;
+        }
+    return ans;
     }
 }
