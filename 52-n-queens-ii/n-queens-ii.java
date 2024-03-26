ef66835e8ef21fb71ee1da915bb0448a9532bc2a
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-03-26 23:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (89.63%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,57 +1,23 @@
 class Solution {
     public int totalNQueens(int n) {
+dfs(n, 0, new boolean[n], new boolean[2 * n - 1], new boolean[2 * n - 1]);
+    return ans;
+  }
 
-        boolean[][] board = new boolean[n][n];
-        return (queens(board, 0));
+  private int ans = 0;
+
+  private void dfs(int n, int i, boolean[] cols, boolean[] diag1, boolean[] diag2) {
+    if (i == n) {
+      ++ans;
+      return;
     }
 
-    public int queens(boolean[][] board, int row)
-    {
-        if( row == board.length)
-        {
-            return 1;
-        }
-        int count = 0;
-
-        //placing the queen and performing the required checks
-        for(int col = 0; col < board.length; col++)
-        {
-            //place the queen if its safe
-            if(isSafe(board,row, col))
-            {
-                board[row][col] = true;
-                count += queens(board, row+1);
-                board[row][col] = false;
-            }
-        }
-        return count;
+    for (int j = 0; j < cols.length; ++j) {
+      if (cols[j] || diag1[i + j] || diag2[j - i + n - 1])
+        continue;
+      cols[j] = diag1[i + j] = diag2[j - i + n - 1] = true;
+      dfs(n, i + 1, cols, diag1, diag2);
+      cols[j] = diag1[i + j] = diag2[j - i + n - 1] = false;
     }
-
-    private static boolean isSafe(boolean[][] board, int row, int col)
-    {
-        // check vertical row
-        for(int i = 0; i < row; i++)
-        {
-            if(board[i][col] == true)
-                return false;
-        }
-
-        //diagonal left
-        int maxLeft = Math.min(row, col);
-        for(int i = 1; i <= maxLeft; i++)
-        {
-            if (board[row-i][col-i])
-                return false;
-        }
-
-        //diagonal right
-        int maxRight = Math.min(row, board.length - col - 1);
-        for(int i = 1; i <= maxRight; i++)
-        {
-            if (board[row-i][col+i])
-                return false;
-        }
-
-        return true;
-    }
+  }
 }
