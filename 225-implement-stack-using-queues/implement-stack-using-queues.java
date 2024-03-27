85add53e4441e42b1273a8846e41e871b8c00891
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-03-28 00:13:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (76.49%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,36 @@
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+    int top ;
+    int del;
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+        top = 0;
+    }
+    
+    public void push(int x) {
+        top = x;
+        q1.add(x);
+    }
+    
+    public int pop() {
+        while(!q1.isEmpty()){
+            del = q1.remove();
+            if(!q1.isEmpty())    q2.add(del);
+        }
+        while(!q2.isEmpty()){
+            top= q2.remove();
+            q1.add(top);
+        }
+        return del;
+    }
+    
+    public int top() {
+        return top;
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
