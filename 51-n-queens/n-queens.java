4cb3e40efe31a4ddbe55bf48fe9c1d815d4eaf10
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-04-07 23:23:09 +0530

    Time: 2 ms (87.88%) | Memory: 44.8 MB (37.07%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,75 +1,81 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-         //create board and initialize with '.'
+        //create board
         char[][] board = new char[n][n];
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board.length; j++) {
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board.length; j++)
+            {
                 board[i][j] = '.';
             }
         }
-        List<List<String>> myBoard = new ArrayList<>();
-        placeQueens(board, 0, myBoard);
-        return myBoard;
+
+        //place queens
+        
+        List<List<String>> boardsWithQueensPlaced = new ArrayList<>();
+        placeQueens(board, 0, boardsWithQueensPlaced);
+        return boardsWithQueensPlaced;
     }
 
-    public void placeQueens(char[][] board,int row, List<List<String>> list)
-    {
-        if(row == board.length) //we have reached the last row 
+    private void placeQueens(char[][] board, int row, List<List<String>> boardsWithQueensPlaced)
+    {   
+        // base 
+        if( row == board.length)
         {
-            list.add(construct(board));
+            boardsWithQueensPlaced.add(construct(board));
             return;
         }
 
-        for(int col = 0; col < board.length; col++)
+        for(int column = 0; column < board.length; column++)
         {
-            //place the queen after checking if its a safe position
-            if(isSafe(board, row, col))
+            if( isSafe(board, row, column))
             {
-            board[row][col] = 'Q';
-            placeQueens(board, row+1, list);
-            //backtrack
-            board[row][col] = '.';
+                board[row][column] = 'Q';
+                placeQueens(board, row+1, boardsWithQueensPlaced);
+                //backtrack
+                board[row][column] = '.';
             }
         }
     }
 
-    public boolean isSafe(char[][] board, int row, int col)
+    private boolean isSafe(char[][] board, int row, int column)
     {
-        //check vertical row 
+        //check vertical row -->
         for(int i = 0; i < row; i++)
         {
-            if(board[i][col] == 'Q')
+            if(board[i][column] == 'Q')
             return false;
         }
 
-        //diagonal left
-        int maxLeft = Math.min(row, col);
+        //check left diagonal
+        int maxLeft = Math.min(row, column);
         for(int i = 1; i <= maxLeft; i++)
         {
-            if (board[row-i][col-i]== 'Q')
-                return false;
+            if (board[row-i][column-i] == 'Q')
+            return false;
         }
 
-        //diagonal right
-        int maxRight = Math.min(row, board.length - col - 1);
+        //check right diagonal
+        int maxRight = Math.min(row, board.length - column - 1);
         for(int i = 1; i <= maxRight; i++)
         {
-            if (board[row-i][col+i] == 'Q')
-                return false;
+            if(board[row-i][column+i] == 'Q')
+            return false;
+            
         }
 
         return true;
     }
 
-    public List<String> construct(char[][] board) {
-
-        List<String> internal = new ArrayList<>();
-        for (int i = 0; i < board.length; i++) {
+    private List<String> construct(char[][] board)
+    {   
+        List<String> rowString = new ArrayList<>();
+        for(int i = 0; i < board.length; i++)
+        {
             String row = new String(board[i]);
-            internal.add(row);
+            rowString.add(row);
         }
-        return internal;
 
+        return rowString;    
     }
-
 }
