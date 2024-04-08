1b394e8ee4fb6cc0d6f393e2deef527b1a22e002
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-04-08 19:25:17 +0530

    Time: 10 ms (42.97%) | Memory: 40.8 MB (64.88%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,55 +1,67 @@
 class Solution {
-    public boolean solveSudoku(char[][] board) {
-       for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-        if (board[i][j] == '.') {
-
-          for (char c = '1'; c <= '9'; c++) {
-            if (isValid(board, i, j, c)) {
-              board[i][j] = c;
-
-              if (solveSudoku(board))
-                return true;
-              else
-                board[i][j] = '.';
-            }
-          }
-
-          return false;
-        }
-      }
+    public void solveSudoku(char[][] board) {
+        // Start solving sudoku from the first cell
+        solve(board, 0, 0);
     }
-    return true;
-  }
 
-  public static boolean isValid(char[][] board, int row, int col, char num) {
-    // check the row
+    private boolean solve(char[][] board, int row, int col) {
+        // Base case: If row is equal to board length, entire board has been filled
+        if (row == board.length) {
+            return true;
+        }
+        
+        // Move to next row when current row is fully filled
+        if (col == board[0].length) {
+            return solve(board, row + 1, 0);
+        }
+        
+        // Skip cells that are already filled
+        if (board[row][col] != '.') {
+            return solve(board, row, col + 1);
+        }
+        
+        // Try different numbers in current cell
+        for (char num = '1'; num <= '9'; num++) {
+            if (isValidPlacement(board, row, col, num)) {
+                board[row][col] = num; // Fill current cell with valid number
+                
+                // Move to next cell
+                if (solve(board, row, col + 1)) {
+                    return true;
+                }
+                
+                // Backtrack to previous state if solution not found
+                board[row][col] = '.';
+            }
+        }
+        
+        // No valid solution found
+        return false;
+    }
+
+    private boolean isValidPlacement(char[][] board, int row, int col, char num) {
+        // Check if num is already in the same row, column or 3x3 subgrid
         for (int i = 0; i < board.length; i++) {
-            // check if the number is in the row
+            // Check row
+            if (board[i][col] == num) {
+                return false;
+            }
+
+            // Check column
             if (board[row][i] == num) {
                 return false;
             }
-        }
-
-        // check the col
-        for (char[] nums : board) {
-            // check if the number is in the col
-            if (nums[col] == num) {
+ 
+            // Check 3x3 subgrid
+            int subgridRow = 3 * (row / 3) + i / 3; // Calculate row index of subgrid
+            int subgridCol = 3 * (col / 3) + i % 3; // Calculate column index of subgrid
+ 
+            if (board[subgridRow][subgridCol] == num) {
                 return false;
             }
         }
 
-        int sqrt = (int)(Math.sqrt(board.length));
-        int rowStart = row - row % sqrt;
-        int colStart = col - col % sqrt;
-
-        for (int r = rowStart; r < rowStart + sqrt; r++) {
-            for (int c = colStart; c < colStart + sqrt; c++) {
-                if (board[r][c] == num) {
-                    return false;
-                }
-            }
-        }
+        // Placement is valid
         return true;
     }
 }
