ae45a3a33a3f0e14fa719fd7fff63f66dfa1acf6
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-04-11 14:38:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (60.75%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.java
+++ 233-number-of-digit-one/number-of-digit-one.java
@@ -1,19 +1,22 @@
 class Solution {
     public int countDigitOne(int n) {
-int count = 0;
-    
-    for (long digit = 1; digit <= n; digit *= 10) 
-    {
-        long before = n / digit;
-        long after = n % digit;
-        count += (before + 8) / 10 * digit;
-        if (before % 10 == 1) 
-        {
-            count += after + 1;
+        int count=0;
+        while(n>9){
+            int digit_count=(int) Math.log10(n);
+           
+            int preceeding_decimal=(int)Math.pow(10.0,digit_count*1.0);
+            
+            int leading_digit=n/preceeding_decimal;
+            count+=((leading_digit)*(Math.pow(10.0,digit_count-1))*digit_count);
+           
+            if(leading_digit>1)
+            count+=preceeding_decimal;
+            if(leading_digit==1){
+                count+=((n%preceeding_decimal)+1);
+            }
+            n=n%preceeding_decimal;
+          
         }
+        return n==0 ? count : count+1;
     }
-    
-    return count;
-  }
-
 }
