d8fe6a68a3e83ff2c2ed2215bf32f2bd77e4494a
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-04-25 15:19:36 +0530

    Time: 1 ms (95.32%) | Memory: 44.5 MB (41.36%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,30 +1,31 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> finalList = new ArrayList<>();
-        List<Integer> permutation = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
 
-        findPermutations(nums, permutation, finalList);
+        findPermutations(nums, finalList, ds, freq);
         return finalList;
     }
 
-    private void findPermutations(int[] nums, List<Integer> makePermutation, List<List<Integer>> allPermutations)
+    private void findPermutations(int[] nums, List<List<Integer>> ans, List<Integer> permutation, boolean[] taken)
     {
         //base statement
-        if (makePermutation.size() == nums.length)
+        if(permutation.size() == nums.length)
         {
-            allPermutations.add(new ArrayList<>(makePermutation));
+            ans.add(new ArrayList<>(permutation));
+            return;
         }
-        else
+        for(int i = 0 ; i < nums.length; i++)
         {
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(makePermutation.contains(nums[i]))
-            continue;
-
-            makePermutation.add(nums[i]);
-            findPermutations(nums, makePermutation, allPermutations);
-            makePermutation.remove(makePermutation.size()-1); //removing last element
+            if(!taken[i])
+            {
+                taken[i] = true;
+                permutation.add(nums[i]);
+                findPermutations(nums, ans, permutation, taken);
+                taken[i] = false;
+                permutation.remove(permutation.size()-1);
+            }
         }
-    }
     }
 }
