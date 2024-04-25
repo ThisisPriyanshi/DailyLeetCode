b979de5cf8521fd82617847cd167580563fd67d7
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-04-25 13:55:59 +0530

    Time: 1 ms (60.46%) | Memory: 42.4 MB (74.87%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> outer = new ArrayList<>();
-        outer.add(new ArrayList<>());
+
+        Arrays.sort(nums); //sorting the array to avoid duplicates
+
+        List<List<Integer>> myList = new ArrayList<>();
+        myList.add(new ArrayList<>()); //added an empty subset
+
         for(int num : nums)
         {
-            int n = outer.size();
-            for(int i =0; i < n; i++)
+            int n = myList.size();
+            for(int i = 0; i < n; i++)
             {
-                List<Integer> internal = new ArrayList<>(outer.get(i));
-                internal.add(num);
-                outer.add(internal);
+                //making the new subsets by including the previous subset
+                List<Integer> mySubList = new ArrayList<>(myList.get(i));
+                mySubList.add(num);
+                myList.add(mySubList);
             }
         }
 
-        return outer;
+        return myList;        
     }
 }
