ec186e4d8ae3914c9c2e8f96f3afeacac13ede0c
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-05-15 16:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (59.68%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -2,19 +2,18 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> outer = new ArrayList<>();
         outer.add(new ArrayList<>());
-        //added empty subset
 
         for(int n : nums)
         {
-            int oSize = outer.size();
-            for(int i = 0; i < oSize; i++)
+            int outerSize = outer.size();
+            for(int i = 0; i < outerSize; i++)
             {
-                List<Integer> internal = new ArrayList<>(outer.get(i));
-                internal.add(n);
-                outer.add(internal);
+                List<Integer> inner = new ArrayList<Integer>(outer.get(i));
+                inner.add(n);
+                outer.add(inner);
             }
         }
-        
+
         return outer;
     }
 }
