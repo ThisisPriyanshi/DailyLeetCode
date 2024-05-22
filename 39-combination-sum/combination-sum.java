2393ca77a7434f4b03036ae78c8c40a3ab40ec41
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-05-22 23:59:37 +0530

    Time: 4 ms (17.99%) | Memory: 44.8 MB (15.50%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+
+        Arrays.sort(candidates);
+        int sumTillNow = 0;
+
+        helper(candidates, 0, target, sumTillNow, subset, result);
+
+        return result;
+    }
+
+    public void helper(int[] arr, int i, int target, int sum, List<Integer> subset, List<List<Integer>> result)
+    {
+        //base statements
+
+        if(sum == target)
+        {
+        result.add(new ArrayList<>(subset));
+        return;
+        }
+
+        if(i >= arr.length)
+        {
+            return;
+        }
+
+        if(sum > target)
+        {
+            return;
+        }
+
+        //skip the elememt
+        helper(arr, i+1, target, sum, subset, result);
+
+        //include the element
+        sum += arr[i];
+
+        subset.add(arr[i]);
+
+        //we stay at thi si only even after including because we can take it multiple times
+        helper(arr, i, target, sum, subset, result);
+
+        //backtrack and remove
+
+        sum -= arr[i];
+        subset.remove(subset.size()-1);
+    }
+}
