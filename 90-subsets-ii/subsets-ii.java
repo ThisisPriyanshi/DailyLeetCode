2b2254e218d0ea8d5ce466b9f5851b05a0510942
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-05-22 23:23:06 +0530

    Time: 1 ms (99.81%) | Memory: 43.4 MB (39.04%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,35 +1,39 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        //same as subsets problem with slight modification
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> mySubset = new ArrayList<>();
 
-        //we need all duplicate elements to be adjacent so we first sort the given array
         Arrays.sort(nums);
-        List<List<Integer>> outer = new ArrayList<>();
-        outer.add(new ArrayList<>());
-        //now we take wo pointers start and end to mark which elements to be picked
-        int start = 0;
-        int end = 0;
-        //since not all elements are to be picked we use for loop
-        for(int i = 0; i < nums.length; i++)
-        {
-            start = 0;
-            //but if there is usplicates the new start will end+1
-            if( i > 0 && (nums[i] == nums[i-1]))
-            {
-                start = end + 1;
-            }
-            end = outer.size()-1;
+        helper(nums, 0, mySubset, ans);
 
-            int oSize = outer.size();
-            for(int j = start; j < oSize; j++)
-            {
-                List<Integer> internal = new ArrayList<>(outer.get(j));
-                internal.add(nums[i]);
-                outer.add(internal);
-            }
+        return ans;
+    }
 
-        }
+    public void helper(int[] arr, int i , List<Integer> subset, List<List<Integer>> result)
+    {
+    //base condition
+    if(i == arr.length)
+    {
+        result.add(new ArrayList<>(subset));
+        return;
+    }
 
-        return outer;
+    //pick the element
+    subset.add(arr[i]);
+
+    //call function
+    helper(arr, i+1, subset, result);
+
+    //backtrack
+    subset.remove(subset.size() - 1);
+
+    //now also ignore duplicates
+    while( (i < arr.length - 1) && (arr[i] == arr[i+1]))
+    {
+        i++;
+    }
+    //now ignore element and call
+    helper(arr, i+1, subset, result);
+    return;
     }
 }
