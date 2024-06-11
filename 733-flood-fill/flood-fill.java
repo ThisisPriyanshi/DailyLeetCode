de7b555c6126436cbc2c6a08f262c4a6ef36ac53
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-06-11 15:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (56.41%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,52 +1,33 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // We calculate the original first using the coordinates (sr , sc)
-        
         int originalColor = image[sr][sc];
-
-        // Incase the oloriginalColor & given Color is same, we can simply return the Original Image
 
         if (originalColor == color)
             return image;
 
-        int m = image.length;
-        int n = image[0].length;
+        int row = image.length;
+        int col = image[0].length;
 
-        flood(sr , sc , image, originalColor, color , m, n);
-
-        // We return the new image[][] matrix after changing all the Neighbouring Cells of (sr , sc) having OldColor to Color
+        fillIt(image, sr, sc, row, col, color, originalColor);
 
         return image;
-
     }
 
-    public void flood(int i, int j, int[][] image, int originalColor, int newColor, int row, int col)
+    private void fillIt(int[][] image, int currRow, int currCol, int row, int col, int color, int original)
     {
-
-        // If we move out of the Matrix Or originalColor is not equal to newColor, we just return back
-
-        if (i < 0 || j < 0 || i == row || j == col || image[i][j] != originalColor)
+        //base statement 
+        if(currRow < 0 || currCol < 0 || currRow == row || currCol == col ||image[currRow][currCol] != original)
+        {
             return;
+        }
 
-        // We change the color of the Current-Cell to newColor
+        image[currRow][currCol] = color;
 
-        image[i][j] = newColor;
+        //call for recusrion ina ll 4 directions
 
-        // Recursive call in Downward direction
-
-        flood(i + 1, j, image, originalColor, newColor, row, col);
-
-        // Recursive call in the Upward direction
-
-        flood(i - 1, j, image, originalColor, newColor, row, col);
-
-        // Recursive call in the Right direction
-
-        flood(i, j + 1, image, originalColor, newColor, row, col);
-
-        // Recursive call in the left direction
-
-        flood(i, j - 1, image, originalColor, newColor, row, col);
+        fillIt(image, currRow+1, currCol, row, col, color, original);
+        fillIt(image, currRow-1, currCol, row, col, color, original);
+        fillIt(image, currRow, currCol-1, row, col, color, original);
+        fillIt(image, currRow, currCol+1, row, col, color, original);
     }
-
 }
