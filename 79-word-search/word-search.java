5db46f319a701f50e8893cc0c65237971ea158a3
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-06-11 19:14:53 +0530

    Time: 154 ms (42.30%) | Memory: 41.4 MB (54.72%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,42 +1,48 @@
 class Solution {
-    public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
+    public boolean exist(char[][] board, String word) 
+    {
+        int tRow = board.length;
+        int tCol = board[0].length;
 
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+        for(int currRow = 0; currRow < tRow; currRow++)
+        {
+            for(int currCol = 0; currCol < tCol; currCol++)
+            {
+                if(board[currRow][currCol] == word.charAt(0))
+                {
+                    if (isPresent(currRow, currCol, tRow, tCol, board, word, 0))
+                    return true;
                 }
             }
         }
-        
         return false;
     }
-    
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
-            return true;
-        }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+
+    private boolean isPresent(int row, int col, int tRow, int tCol, char[][] board, String word, int k)
+    {
+        //base statement
+
+        if(k == word.length())
+        return true ;
+
+        if(row == tRow || col == tCol || row < 0 || col < 0 || board[row][col] != word.charAt(k))
+        {
             return false;
         }
-        
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
+
+        char ch = board[row][col];
+        //we are considering this block so we mark it
+        board[row][col] = '#';
+
+        //call recursion to take things forward
+        boolean ans = isPresent(row+1, col, tRow, tCol, board, word, k+1);
+        boolean ans1 = isPresent(row-1, col, tRow, tCol, board, word, k+1);
+        boolean ans2 = isPresent(row, col+1, tRow, tCol, board, word, k+1);
+        boolean ans3 = isPresent(row, col-1, tRow, tCol, board, word, k+1);
+
+        board[row][col] = ch;
+
+        return ( ans || ans1 || ans2 || ans3);
+
     }
 }
