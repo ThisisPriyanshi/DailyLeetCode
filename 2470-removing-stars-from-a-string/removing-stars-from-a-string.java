c0e68b6e56546d4be449454bf11d3a41b6eef7ed
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-06-24 00:34:40 +0530

    Time: 10 ms (97.89%) | Memory: 45.4 MB (79.45%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String removeStars(String s) {
+        char[] c = s.toCharArray();
+        int i = 0;
+        int j = 0;
+
+        while(j < s.length()){
+            if(c[j] == '*') i--;
+            else c[i++] = c[j];
+            j++;
+        }
+        return new String(c, 0 , i);
+    }
+}
