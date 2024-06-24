1b7e3afa4e459f7e3f6db55d6ba965dcd4b3b00a
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-06-24 17:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (82.54%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,36 +1,51 @@
 class MyStack {
-    Queue<Integer> q1;
-    Queue<Integer> q2;
-    int top ;
-    int del;
+
+    Queue <Integer> myQueue;
+    Queue <Integer> myQueue2;
+
     public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
-        top = 0;
+
+        myQueue = new LinkedList<>();
+        myQueue2 = new LinkedList<>();
     }
     
     public void push(int x) {
-        top = x;
-        q1.add(x);
+
+        while(!myQueue.isEmpty())
+        {
+            myQueue2.add(myQueue.remove());
+        }
+
+        myQueue.add(x);
+
+        while(!myQueue2.isEmpty())
+        {
+            myQueue.add(myQueue2.remove());
+        }
+
+        
     }
     
     public int pop() {
-        while(!q1.isEmpty()){
-            del = q1.remove();
-            if(!q1.isEmpty())    q2.add(del);
-        }
-        while(!q2.isEmpty()){
-            top= q2.remove();
-            q1.add(top);
-        }
-        return del;
+
+        return myQueue.remove();
+        
     }
     
     public int top() {
-        return top;
+        return myQueue.peek();
     }
     
     public boolean empty() {
-        return q1.isEmpty();
+        return myQueue.isEmpty();
     }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
