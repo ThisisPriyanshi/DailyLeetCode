3c067f11b2f6841e765b9738797f6db4e8720625
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-07-23 23:56:07 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (98.20%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,16 +1,40 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-        int[] count = new int[201];
-        for (int num : nums) {
-            count[num + 100]++;
+        int[] count=new int[202];
+        for(int i=0;i<nums.length;i++){
+            count[nums[i]+100]++;
         }
-        Integer[] numsArr = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        Arrays.sort(numsArr, (a, b) -> {
-            if (count[a + 100] == count[b + 100]) {
-                return b - a;
+        quickSort(nums,count,0,nums.length-1);
+        return nums;
+    }
+    void quickSort(int[] nums,int freq[],int low,int high){
+        if(low<high){
+            int pivot=partition(nums,freq,low,high);
+            quickSort(nums,freq,low,pivot-1);
+            quickSort(nums,freq,pivot+1,high);
+        }
+    }
+    int partition(int[] nums,int freq[],int low,int high){
+        int pivot=freq[nums[high]+100];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(pivot > freq[nums[j]+100]){
+                i++;
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+            }else if(freq[nums[j]+100]==pivot){
+                if(nums[high]<=nums[j]){
+                    i++;
+                    int temp=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=temp;
+                }
             }
-            return count[a + 100] - count[b + 100];
-        });
-        return Arrays.stream(numsArr).mapToInt(Integer::intValue).toArray();
+        }
+        int temp=nums[i+1];
+        nums[i+1]=nums[high];
+        nums[high]=temp;
+        return i+1;
     }
 }
