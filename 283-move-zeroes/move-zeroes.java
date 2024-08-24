5f7229947ebe3e6afb961ed7cb5a7672de926d27
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-08-24 15:06:32 +0530

    Time: 2 ms (82.30%) | Memory: 45.8 MB (62.22%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-int left = 0;
+        //using two pointers
 
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] != 0) {
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
-                left++;
+        int read = 0;
+        int write = 0;
+
+        while(read < nums.length)
+        {
+            if(nums[read] != 0)
+            {
+                nums[write] = nums[read];
+                write++;
             }
-        }        
+
+            read++;
+        }
+
+        while(write < nums.length)
+        {
+            nums[write] = 0;
+            write++;
+        }
     }
 }
