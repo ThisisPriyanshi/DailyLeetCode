e03611692b325e61e3047558e791e2d3d21075b8
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-08-24 15:00:22 +0530

    Time: 2 ms (82.30%) | Memory: 45.8 MB (75.48%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,45 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+int left = 0;
 
-        //not optimized but simple for beiginners
-        // int countZero = 0;
-        // //we maintain a zerocounter and do changes accordingly
-        // for(int i = 0; i<nums.length ; i++)
-        // {
-        //     if(nums[i] == 0)
-        //     {
-        //         countZero++;
-        //     }
-        //     else if(countZero>0)
-        //     {
-        //         int t = nums[i];
-        //         nums[i] = 0;
-        //         nums[i-countZero] = t;
-        //     }
-        // }
-
-
-        //optimized two pointer
-        int readptr = 0;
-        int writeptr = 0;
-        while(readptr < nums.length)
-        {
-            if(nums[readptr] != 0)
-            {
-                nums[writeptr] = nums[readptr];
-                writeptr++;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
-
-            readptr++;
-        }
-
-        while(writeptr < nums.length)
-        {
-            nums[writeptr] = 0;
-            writeptr++;
-        }
-
-        //System.out.println(Arrays.toString(nums));
-        
+        }        
     }
 }
