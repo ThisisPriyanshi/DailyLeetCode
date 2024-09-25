3e8eb908233e1acb02b18dea265f8b32c4922753
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-09-25 13:26:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (82.36%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,61 +1,36 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        int maxUnqiueCharCount =  getUniqueCharCount(s);
-        int [] freqMap = new int [26];
-        int result = 0;
-        
-        for (int currentUniqueChar = 1;  currentUniqueChar <= maxUnqiueCharCount; currentUniqueChar++) {
-            Arrays.fill(freqMap, 0);
-            int uniqueCount = 0, countAtLeastK = 0, windowStart  = 0, windowEnd = 0;
-            
-            while (windowEnd < s.length()) {
-                
-                if (uniqueCount <= currentUniqueChar) {
-                    int idx = s.charAt(windowEnd) - 'a';
-                    if (freqMap[idx] == 0) {
-                        uniqueCount++;
-                    }
-                    freqMap[idx]++;
-                    
-                    if (freqMap[idx] == k) {
-                        countAtLeastK++;
-                    }
-                    windowEnd++;
-                } else {
-                    // We need to  shrink
-                    int idx = s.charAt(windowStart) - 'a';
-                    if (freqMap[idx] == k) {
-                        countAtLeastK--;
-                    }
-                    freqMap[idx]--;
-                    if (freqMap[idx] == 0) {
-                        uniqueCount--;
-                    }
-                    windowStart++;
-                }
-                
-                if (uniqueCount == currentUniqueChar && uniqueCount == countAtLeastK) {
-                    result = Math.max(windowEnd - windowStart, result);
-                }
-            }
-            
-        }
-        
-        return result;
+        return helper(s.toCharArray(),0, s.length(),k);
     }
-    
-    private int getUniqueCharCount(String s) {
-        boolean [] chars = new boolean [26];
-        int uniqueCount = 0;
-        
-        for (char ch : s.toCharArray()) {
-            int idx  = ch - 'a';
-            if (!chars[idx]) {
-                uniqueCount++;
-                chars[idx] = true;
+
+    public int helper(char[] myArr, int start, int end, int k)
+    {
+        if(end- start < k)
+        return 0;
+
+        int[] count = new int[26];//only lowercase letters
+        for(int i = start; i < end; i++)
+        {
+            count[myArr[i] - 'a']++;
+        }
+
+        //now i have counted all occurences we look for first invalid and second invalid and then return their difference as the longestvalid
+
+        for(int i = start; i < end; i++)
+        {
+            if(count[myArr[i] - 'a'] < k)
+            {
+                int j = i+1;
+
+                while(j < end && count[myArr[j]-'a'] < k)
+                {
+                    j++;
+                }
+
+                return Math.max(helper(myArr,start,i,k), helper(myArr,j, end,k));
             }
         }
-        
-        return uniqueCount;
+
+        return end-start;
     }
 }
