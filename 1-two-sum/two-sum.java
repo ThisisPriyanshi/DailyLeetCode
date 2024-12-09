f9f318dbf13590f7365a02dae34d2a33a8b62aeb
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2024-12-09 10:32:03 +0530

    Time: 2 ms (98.91%) | Memory: 45 MB (38.11%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -5,14 +5,15 @@ class Solution {
         for(int i = 0; i < nums.length; i++)
         {
             int n2 = target - nums[i];
-            if( myMap.containsKey(n2))
+            if(myMap.containsKey(n2))
             {
-            return new int[]{i, myMap.get(n2)};
+                return new int[]{i, myMap.get(n2)};
             }
-
-             myMap.put(nums[i], i);
+            else
+            {
+                myMap.put(nums[i], i);
+            }
         }
-
         return new int[]{0,0};
     }
 }
