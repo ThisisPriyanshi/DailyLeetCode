546d2812ab19785e5d43e054fc093e8070dfd77c
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2025-01-03 18:03:08 +0530

    Time: 1 ms (94.64%) | Memory: 45.4 MB (57.54%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -3,22 +3,19 @@ class Solution {
         int totSum = 0;
         int leftSum = 0;
 
-        for( int ele : nums)
-        {
-            totSum += ele;
-        }
+        for(int ele : nums)
+         totSum += ele;
 
-        for(int i = 0; i < nums.length; i++)
-        {
-            if( leftSum * 2 == totSum - nums[i])
+         for(int i = 0; i < nums.length; i++)
+         {
+            if(leftSum * 2 == totSum - nums[i])
             {
-                return i;
+            return i;
             }
             else
-            {
-                leftSum += nums[i];
-            }
-        }
-        return -1;
+            leftSum += nums[i];
+         }
+
+         return -1;
     }
 }
