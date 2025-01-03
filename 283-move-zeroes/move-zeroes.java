629e221a7fa457a05f50505618412c4a50d105db
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2025-01-03 15:54:26 +0530

    Time: 1 ms (99.82%) | Memory: 45.9 MB (68.37%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,22 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n= nums.length;
-         int snowBallsize=0;
-         for(int i=0 ; i<n ; i++){
-             if(nums[i]==0){
-                 snowBallsize++;
-            }
-             else if(snowBallsize>0){
-                 int t= nums[i];
-                 nums[i]=0;
-                 nums[i- snowBallsize]= t;
-             }
-        }
+       int read = 0;
+       int write = 0;
+
+       while(read < nums.length)
+       {
+         if( nums[read] != 0)
+         { 
+            nums[write] = nums[read];
+            write++;
+         }
+         read++;
+       }
+
+       while(write < nums.length)
+       {
+        nums[write] = 0;
+        write++;
+       }
     }
 }
